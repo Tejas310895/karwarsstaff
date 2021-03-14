2e2ec69a7b9770d9413653b286c473e12e3fd275
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-03-14 21:34:04 +0530

    error

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ include("includes/db.php");
         <div class="container">
             <ul class="nav page-navigation">
               <li class="nav-item <?php if(isset($_GET['dashboard'])){echo "active";} ?>">
-                <a class="nav-link" href="index.?dashboard">
+                <a class="nav-link" href="index.php?dashboard">
                   <i class="mdi mdi-file-document-box menu-icon"></i>
                   <span class="menu-title">Dashboard</span>
                 </a>
@@ -86,13 +86,13 @@ include("includes/db.php");
                   </a>
                   <div class="submenu">
                       <ul>
-                          <li class="nav-item"><a class="nav-link" href="index.?order_new">Generate Order</a></li>
-                          <li class="nav-item"><a class="nav-link" href="index.?order_old">Modify Order</a></li>
+                          <li class="nav-item"><a class="nav-link" href="index.php?order_new">Generate Order</a></li>
+                          <li class="nav-item"><a class="nav-link" href="index.php?order_old">Modify Order</a></li>
                       </ul>
                   </div>
               </li>
               <li class="nav-item <?php if(isset($_GET['reports'])){echo "active";} ?>">
-                  <a href="index.?reports" class="nav-link reports">
+                  <a href="index.php?reports" class="nav-link reports">
                     <i class="mdi mdi-chart-areaspline menu-icon"></i>
                     <span class="menu-title">Reports</span>
                     <i class="menu-arrow"></i>
